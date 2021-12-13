d9d1eb562dddc65180d4ecee8094aaac34bfa73b
Gabriel <gabrielcpf2997@gmail.com>
2021-12-12 18:35:38 -0800

    changed package.json and added module for app.js, ready for deploy

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://admin:first_express@cluster0.uqjuw.mongodb.net/local_library?retryWrites=true&w=majority';
+const dev_db_url = 'mongodb+srv://admin:first_express@cluster0.uqjuw.mongodb.net/local_library?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
